dc269ea501e696639dfce5494bb1438d07c8d74b
sheltonn <norris.shelton@gmail.com>
2024-10-14 23:19:10 -0400

    Adding get single, get list, paging and sorting.  JUnit tests run in parallel.

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -1,5 +1,8 @@
 package example.cashcard;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.SortDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,10 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/cashcards")
 class CashCardController {
+
     private final CashCardRepository cashCardRepository;
 
     private CashCardController(CashCardRepository cashCardRepository) {
@@ -36,5 +41,10 @@ class CashCardController {
                 .buildAndExpand(savedCashCard.id())
                 .toUri();
         return ResponseEntity.created(locationOfNewCashCard).build();
+    }
+
+    @GetMapping
+    private ResponseEntity<List<CashCard>> findAll(@SortDefault(sort = "amount", direction = Sort.Direction.ASC) Pageable pageable) {
+        return ResponseEntity.ok(cashCardRepository.findAll(pageable).getContent());
     }
 }
